180a85398ba371e3ef01ffb6dd8ef7824bf57746
Lang149 <lli895300@gmail.com>
2024-09-19 09:25:32 -0700

    Update Shipping Account Program.py

--- Shipping Account Program.py
+++ Shipping Account Program.py
@@ -1,10 +1,12 @@
 print("Welcome to the Shipping Account Program!")
-username = input("Hello! What is your username!")
-Shipping_Price = [5.10, 4.95, 4.60, 4.30]
-
-    if username = ("Lang", "Yamshippy", "James", "Letsparty99"):
-
-
+username = input("Hello! What is your username: ")
+valid_usernames = ["Lang221", "Yamshippyhappyface", "JamesIsTrueForYou", "Letsparty99", "Andy123"] #Chatgpt
+if username in valid_usernames:
+    capitalized_username =[username.capitalize() for username in username]
+    print("Hello " + username + ". Welcome back to your account.")
+    print("The current shipping prices are as follows:")
+else:
+    print("Sorry, you do not have an account with us. Goodbye.")
         if 0 < Shipping_Amount <= 200:
             Total = Shipping_Price[1] * Shipping_Amount
         elif 200 < Shipping_Amount <= 400:
