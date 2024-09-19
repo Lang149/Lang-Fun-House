d4c6a40370d01f1c0e1d4dcb65f02498e04904b4
AndyChen1023 <chenandy5967@gmail.com>
2024-09-19 09:29:02 -0700

    Update Shipping Account Program.py

--- Shipping Account Program.py
+++ Shipping Account Program.py
@@ -1,6 +1,7 @@
 print("Welcome to the Shipping Account Program!")
 username = input("Hello! What is your username: ")
 valid_usernames = ["Lang221", "Yamshippyhappyface", "JamesIsTrueForYou", "Letsparty99", "Andy123"] #Chatgpt
+Shipping_Price = [5.10, 4.95, 4.60, 4.30]
 if username in valid_usernames:
     capitalized_username =[username.capitalize() for username in username]
     print("Hello " + username + ". Welcome back to your account.")
