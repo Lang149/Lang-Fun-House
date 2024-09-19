2fd1f09c93e28d52c05082c851672552b135f171
AndyChen1023 <chenandy5967@gmail.com>
2024-09-19 09:24:34 -0700

    Update Shipping Account Program.py

--- Shipping Account Program.py
+++ Shipping Account Program.py
@@ -1,3 +1,17 @@
 print("Welcome to the Shipping Account Program!")
 username = input("Hello! What is your username!")
-    if username = ("Lang" + "Yamshippy" + "James" + "Letsparty99")
+Shipping_Price = [5.10, 4.95, 4.60, 4.30]
+
+    if username = ("Lang", "Yamshippy", "James", "Letsparty99"):
+
+
+        if 0 < Shipping_Amount <= 200:
+            Total = Shipping_Price[1] * Shipping_Amount
+        elif 200 < Shipping_Amount <= 400:
+            Total = Shipping_Price[2] * Shipping_Amount
+        elif 400 < Shipping_Amount <= 800:
+            Total = Shipping_Price[3] * Shipping_Amount
+        else:
+            Total = Shipping_Price[4] * Shipping_Amount
+            
+            
