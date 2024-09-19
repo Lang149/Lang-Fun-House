45c29a5cc72e56390e5730b9260e2ccd0e7fd95b
Lang149 <lli895300@gmail.com>
2024-09-19 09:31:57 -0700

    Update Shipping Account Program.py

--- Shipping Account Program.py
+++ Shipping Account Program.py
@@ -7,15 +7,18 @@ if username in valid_usernames:
     print("Hello " + username + ". Welcome back to your account.")
     print("The current shipping prices are as follows:")
     Shipping_Amount = input(f"Hello! {username}, What amount of _ would you like to buy?")
-     if 0 < Shipping_Amount <= 200:
-            Total = Shipping_Price[1] * Shipping_Amount
-        elif 200 < Shipping_Amount <= 400:
-            Total = Shipping_Price[2] * Shipping_Amount
-        elif 400 < Shipping_Amount <= 800:
-            Total = Shipping_Price[3] * Shipping_Amount
-        else:
-            Total = Shipping_Price[4] * Shipping_Amount
+    if 0 < Shipping_Amount <= 200:
+        Total = Shipping_Price[1] * Shipping_Amount
+        print(Total)
+    elif 200 < Shipping_Amount <= 400:
+        Total = Shipping_Price[2] * Shipping_Amount
+        print(Total)
+    elif 400 < Shipping_Amount <= 800:
+        Total = Shipping_Price[3] * Shipping_Amount
+        print(Total)
+    else:
+        Total = Shipping_Price[4] * Shipping_Amount
+        print(Total)
 else:
     print("Sorry, you do not have an account with us. Goodbye.")
-      
             
