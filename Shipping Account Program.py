bdd11612861486dd9b9d791052809ae032ecd5f5
AndyChen1023 <chenandy5967@gmail.com>
2024-09-23 08:50:50 -0700

    Update Shipping Account Program.py

--- Shipping Account Program.py
+++ Shipping Account Program.py
@@ -1,27 +1,54 @@
 print("Welcome to the Shipping Account Program!")
 username = input("Hello! What is your username: ")
-valid_usernames = ["Lang221", "Yamshippyhappyface", "JamesIsTrueForYou", "Letsparty99", "Andy123"] #Chatgpt
+valid_usernames = ["Lang221", "Yamshippyhappyface",
+                   "JamesIsTrueForYou", "Letsparty99", "Andy123"]  # Chatgpt
 Shipping_Price = [5.10, 4.95, 4.60, 4.30]
+
+
+def Shipping_Total(Amount):
+    if 0 < Amount <= 100:
+        Total = Shipping_Price[0] * Amount
+    elif 100 < Amount <= 500:
+        Total = Shipping_Price[1] * Amount
+    elif 500 < Amount <= 1000:
+        Total = Shipping_Price[2] * Amount
+    elif Amount > 1000:
+        Total = Shipping_Price[3] * Amount
+    else:
+        print(f"The value \"{Amount}\" is not a possible value.")
+        Total = 0
+    return Total
+
+
+def confirmation(confirm, cancel):
+    while True:
+        print(f"Type {confirm} to confirm, type {cancel} to cancel.")
+        confirmation_input = input("Input: ").upper()
+        if confirmation_input == confirm:
+            print(
+                f"Your shipping order of {Shipping_Amount}, costing {Shipping_Total(Shipping_Amount)}$ has been sent, please check your email for the receipt.")
+            break
+        elif confirmation_input == cancel:
+            print(
+                f"Your shipping order of {Shipping_Amount}, costing {Shipping_Total(Shipping_Amount)}$ has been canceled.")
+            break
+        else:
+            print("Your answer is not a vaild answer please try again.")
+
+
 if username in valid_usernames:
-    capitalized_username =[username.capitalize() for username in username]
+    capitalized_username = [username.capitalize() for username in username]
     print("Hello " + username + ". Welcome back to your account.")
     print("The current shipping prices are as follows:")
     print("Shipping orders 0 to 100: $5.10 each")
     print("Shipping orders 100 to 500: $5.00 each")
     print("Shipping orders 500 to 1000: $4.95 each")
     print("Shipping orders over 1000: $4.80 each")
-    Shipping_Amount = input("Hello!" + username + " How much shipping orders would you like to buy? ")
-    if 0 < int(Shipping_Amount) <= 200:
-        Total = Shipping_Price[1] * int(Shipping_Amount)
-        print(Total)
-    elif 200 < int(Shipping_Amount) <= 400:
-        Total = Shipping_Price[2] * int(Shipping_Amount)
-        print(Total)
-    elif 400 < int(Shipping_Amount) <= 800:
-        Total = Shipping_Price[3] * int(Shipping_Amount)
-        print(Total)
-    else:
-        Total = Shipping_Price[4] * int(Shipping_Amount)
-        print(Total)
+    print("Hello!" + username +
+          " How much shipping orders would you like to buy? ")
+    Shipping_Amount = int(input("Shipping Order amount: "))
+    print(f"Your shipping price is {Shipping_Total(Shipping_Amount)}$.")
+    confirmation("Y", "N")
+
 else:
     print("Sorry, you do not have an account with us. Goodbye.")
